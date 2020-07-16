ee10312e640ee504ad1885f97ad4f62a28a30087
ArtistDeveloper <40491724+ArtistDeveloper@users.noreply.github.com>
2020-07-16 23:23:26 +0900

    1924 solve

---
+++ 1924.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+
+int main()
+{
+	int month, day, totalDay = 0;
+	
+	scanf("%d %d", &month, &day);
+
+	int dayArray[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	for (int i = 0; i < month; i++) {
+		totalDay += dayArray[i];
+	}
+
+	totalDay += day;
+
+	switch (totalDay % 7)
+	{
+	case 0:
+		printf("SUN");
+		break;
+	case 1:
+		printf("MON");
+		break;
+	case 2:
+		printf("TUE");
+		break;
+	case 3:
+		printf("WED");
+		break;
+	case 4:
+		printf("THU");
+		break;
+	case 5:
+		printf("FRI");
+		break;
+	case 6:
+		printf("SAT");
+		break;
+	default:
+		break;
+	}
+	
+	return 0;
+}
