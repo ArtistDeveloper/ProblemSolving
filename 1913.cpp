2c3aba1d61d7a08289603b7ac09e80d8e6a2a404
ArtistDeveloper <40491724+ArtistDeveloper@users.noreply.github.com>
2020-08-19 03:37:11 +0900

    1913 Solve

--- 1913.cpp
+++ 1913.cpp
@@ -2,44 +2,67 @@
 #include <cstdio>
 
 int array[999+1][999+1];
-int count = 1;
 int row = 0, col = 0;
-int flag = 1;
-int pos;
+int downRowLimit, rightColLimit, upRow = 0, leftCol = 0;
 
-void draw(int n)
+
+void numPut(int n)
 {
-	for (int k = n; k > n; k--) {
-		if (flag == 1) {
-			for (int i = k; i > 0; i--) {
-				array[row][col] = count;
+	downRowLimit = n-1;
+	rightColLimit = n-1;
+
+	for (int k = n * n; k > 0; k--) {
+		if (row < downRowLimit && col == leftCol) {
+			array[row++][col] = k;
+		}
+
+		else if (col < rightColLimit && row == downRowLimit) {
+			array[row][col++] = k;
+		}
+
+		else if (upRow < row && col == rightColLimit) {
+			array[row--][col] = k;
+		}
+
+		else if (row == upRow && leftCol < col) {
+			array[row][col--] = k;
+			if (col == leftCol + 1) {
+				downRowLimit -= 1;
+				rightColLimit -= 1;
+				leftCol += 1;
+				upRow += 1;
 			}
 		}
 
-		else if (flag == 0) {
-			for (int j = k; j > 0; j--) {
-				array[row][col] = count;
-			}
+		else if (upRow == leftCol) {
+			array[row][col] = k;
 		}
 	}
 }
 
-void arrPrint(int size)
+void arrPrint(int size, int posNum)
 {
+	int row, col;
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
+			if (array[i][j] == posNum) {
+				row = i;
+				col = j;
+			}
 			printf("%d", array[i][j]);
+			printf(" ");
 		}
 		printf("\n");
 	}
+	printf("%d %d", row+1, col+1);
 }
 
 int main()
 {
-	int n;
-	scanf("%d", &n);
+	int n, posNum;
+	scanf("%d %d", &n, &posNum);
 	
-	draw(n);
-	arrPrint(n);
+	numPut(n);
+	arrPrint(n, posNum);
 	return 0;
 }
