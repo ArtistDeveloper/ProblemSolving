701597a96802dc5deeea25282a03d3808eafce10
ArtistDeveloper <40491724+ArtistDeveloper@users.noreply.github.com>
2021-02-03 23:45:12 +0900

    Create 2231.cpp
    solving

---
+++ 2231.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, m; //m은 생성자.
+	int count = 0;
+	int rest = 0, quotient = 0;
+	cin >> n;
+
+	while (count < n) {
+		//while (quotient / 0 != 0) {
+
+		//}
+
+		
+
+		//숫자 끝까지 돈 것.
+		if (quotient / 0 != 0) {
+			
+		}
+	}
+
+	return 0;
+}
