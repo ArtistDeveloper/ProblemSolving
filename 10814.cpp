ca100b05de7bc7fe2ed3408df0ee9f31d69279c5
ArtistDeveloper <40491724+ArtistDeveloper@users.noreply.github.com>
2021-03-14 06:34:26 +0900

    10814 solved

--- 10814.cpp
+++ 10814.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+bool compare(pair<int, string> a, pair<int, string> b)
+{
+	return a.first < b.first;
+}
+
+int main()
+{
+	int n, age;
+	string name;
+	vector<pair<int, string>> member;
+	
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> age >> name;
+		member.push_back({age, name});
+	}
+
+	stable_sort(member.begin(), member.end(), compare);
+
+	for (int i = 0; i < n; i++) {
+		cout << member[i].first << ' ' << member[i].second << '\n';
+	}
+
+	return 0;
+}
